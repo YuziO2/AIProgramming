5fa331cb23edb8ea28d0da935e346e3deb1ff17d
Yuzi0201 <xiaoyuzi2017@gmail.com>
2022-09-27 08:35:36 +0800

    EX2 finished

--- EX2/src/App.vue
+++ EX2/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { reactive, ref, onMounted } from "vue";
+import { ref, onMounted } from "vue";
+import { cloneDeep } from 'lodash'
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Canvas from "./components/Canvas.vue";
@@ -9,8 +10,10 @@ const pointsNumber = 10; //城市数量
 const points = []; //城市
 const distances = []; //距离矩阵
 const groupsNumber = 100; //种群数量
-const groups = []; //种群
-const iterationTimes = 1000; //迭代次数
+let groups = []; //种群
+const sonNumber = 32;
+const sonGroup = [];
+let iterationTimes = 1000; //迭代次数
 const crossRate = 0.9; //交叉率
 const mutationRate = 0.1; //变异率
 //路线类，route:Array;fitness,surviveProbability,distances:Number
@@ -61,116 +64,132 @@ for (let i = 0; i < groupsNumber; i++) {
 }
 //计算初始种群中每个个体的适应度及生存概率
 //适应度为序列中相邻两城之间的距离之和的倒数
-function calculateGroup() {
+function calculateGroup(group) {
   let totalFitness = 0;
-  for (let i = 0; i < groupsNumber; i++) {
+  for (let i = 0; i < group.length; i++) {
     let totalDistance = 0;
     for (let j = 0; j < pointsNumber - 1; j++)
-      totalDistance += distances[groups[i].route[j]][groups[i].route[j + 1]];
-    groups[i].distance = totalDistance;
-    groups[i].fitness = 1 / totalDistance; //个体的适应度为其总距离的倒数
-    totalFitness += groups[i].fitness;
+      totalDistance += distances[group[i].route[j]][group[i].route[j + 1]];
+    group[i].distance = totalDistance;
+    group[i].fitness = 1 / totalDistance; //个体的适应度为其总距离的倒数
+    totalFitness += group[i].fitness;
   }
   //计算每个个体的生存概率（被选择概率）,为个体适应度 / 总适应度
-  for (let i = 0; i < groupsNumber; i++)
-    groups[i].surviveProbability = groups[i].fitness / totalFitness;
+  for (let i = 0; i < group.length; i++)
+    group[i].surviveProbability = group[i].fitness / totalFitness;
 }
-calculateGroup();
+calculateGroup(groups);
 
 //对生成的groups进行排序
 groups.sort((a, b) => b.surviveProbability - a.surviveProbability);
+const initGroup = cloneDeep(groups)
 const firstRoute = ref(groups[0].route); //并传给canvas进行绘制
 
 const buttonMessage = ref("开始计算");
+
+//计算累计的概率，利用赌轮选择法，随机生成0~1之间一个数，根据计算出来的累计概率选择个体
+function selectPoint() {
+  let totalProbability = 0
+  for (let i = 0; i < groupsNumber; i++) {
+    totalProbability += groups[i].surviveProbability
+    if (Math.random() < totalProbability)
+      return i
+  }
+}
+
 function calculate() {
+  iterationTimes = prompt("请输入迭代次数，默认1000：", 1000)
+  while (!/^[0-9]*[1-9][0-9]*$/.test(iterationTimes)) {
+    alert("非法输入！请输入正整数！")
+    iterationTimes = prompt("请输入迭代次数，默认1000：", 1000)
+  }
+  debugger
+  iterationTimes = parseInt(iterationTimes)
+  groups = cloneDeep(initGroup)
   buttonDisabled.value = true;
   buttonMessage.value = "计算中，请稍后……"
-  for (let i = 0; i < iterationTimes; i++) {
-    //计算适应度以及生存概率
-    calculateGroup();
-    //选择开始
-    //计算累计的概率
-    const totalProbability = new Array(groupsNumber);
-    totalProbability[0] = groups[0].surviveProbability;
-    for (let j = 1; j < groupsNumber; j++) {
-      totalProbability[j] =
-        totalProbability[j - 1] + groups[j].surviveProbability;
-    }
-    //记录被选择的个体，利用赌轮选择法，随机生成0~1之间一个数，根据计算出来的累计概率选择个体
-    let selectedGroups = new Array(groupsNumber);
-    for (let j = 0; j < groupsNumber; j++) {
-      for (let k = 0; k < groupsNumber; k++) {
-        if (Math.random() <= totalProbability[k]) {
-          selectedGroups[j] = groups[k];
-          break;
-        }
-      }
-    }
-    //被选择的种群覆盖初始种群
-    for (let j = 0; j < groupsNumber; j++) {
-      groups[j] = selectedGroups[j];
-    }
-    //选择结束，交叉开始
-    //第k（k=0、2、4、...、2n）个个体和k+1个个体有一定的概率交叉变换
-    //设置一个0~1之间的随机数，若在Pc（交配率）范围内，则该该个体k与下一个个体k+1进行交配
-    //随机生成子代交配时DNA交换的数量(1~pointsNumber / 2)
-    let changeNumber = Math.floor(
-      ((Math.random() * pointsNumber * 10) % (pointsNumber / 2)) + 1
-    );
-    //交叉
-    for (let j = 0; j < groupsNumber - 1; j += 2) {
-      //在交叉率以内，则该个体i与下一个个体i+1进行交叉
-      if (Math.random() < crossRate) {
-        let changePoint = Math.floor((Math.random() * pointsNumber * 10) % (pointsNumber - changeNumber))  //交叉点
-        let map = new Map()
-        for (let k = changePoint; k < changeNumber + changePoint; k++) {//建立不重复的映射
-          let temp1 = groups[j].route[k]
-          let temp2 = groups[j + 1].route[k]
-          groups[j].route[k] = temp2
-          groups[j + 1].route[k] = temp1//交换
-          if (map.has(temp1)) {
-            temp1 = map.get(temp1)
+  setTimeout(() => {
+    for (let i = 0; i < iterationTimes; i++) {
+      for (let j = 0; j < sonNumber; j++) {
+        //选择开始
+        let fatherIndex = selectPoint()
+        let matherIndex = selectPoint()
+        while (fatherIndex == matherIndex)//防止自交
+          matherIndex = selectPoint()
+        let Father = cloneDeep(groups[fatherIndex])
+        let Mather = cloneDeep(groups[matherIndex])
+        //选择结束，交叉开始
+        //在交叉率以内，则交叉
+        if (Math.random() < crossRate) {
+          let changeNumber = Math.floor(
+            ((Math.random() * pointsNumber * 10) % (pointsNumber / 2)) + 1
+          );
+          let changePoint = Math.floor((Math.random() * pointsNumber * 10) % (pointsNumber - changeNumber))  //交叉点
+          let map = new Map()
+          for (let k = changePoint; k < changeNumber + changePoint; k++) {//建立不重复的映射
+            let temp1 = Father.route[k]
+            let temp2 = Mather.route[k]
+            Father.route[k] = temp2
+            Mather.route[k] = temp1//交换
+            if (map.has(temp1)) {
+              temp1 = map.get(temp1)
+            }
+            if (map.has(temp2)) {
+              temp2 = map.get(temp2)
+            }
+            map.set(temp1, temp2)
+            map.set(temp2, temp1)
           }
-          if (map.has(temp2)) {
-            temp2 = map.get(temp2)
+          //利用映射解决基因冲突问题
+          for (let k = 0; k < pointsNumber; k++) {
+            if (k >= changePoint && k < changeNumber + changePoint)
+              continue
+            if (map.has(Father.route[k]))
+              Father.route[k] = map.get(Father.route[k])
+            if (map.has(Mather.route[k]))
+              Mather.route[k] = map.get(Mather.route[k])
           }
-          map.set(temp1, temp2)
-          map.set(temp2, temp1)
-        }
-        //利用映射解决基因冲突问题
-        for (let k = 0; k < pointsNumber; k++) {
-          if (k >= changePoint && k < changeNumber + changePoint)
-            continue
-          if (map.has(groups[j].route[k]))
-            groups[j].route[k] = map.get(groups[j].route[k])
-          if (map.has(groups[j + 1].route[k]))
-            groups[j + 1].route[k] = map.get(groups[j + 1].route[k])
+          sonGroup.push(Father)
+          sonGroup.push(Mather)
         }
       }
-    }
-    //变异
-    //每个实例都有可能基因（路径）多次变换
-    for (let j = 0; j < groupsNumber; j++) {
-      if (Math.random() < mutationRate) {
-        for (let exchangeTime = Math.floor(Math.random() * pointsNumber + 1); exchangeTime > 0; exchangeTime--) {
-          const changePointA = Math.floor(Math.random() * pointsNumber)
-          const changePointB = Math.floor(Math.random() * pointsNumber)
-          const temp = groups[j].route[changePointA]
-          groups[j].route[changePointA] = groups[j].route[changePointB]
-          groups[j].route[changePointB] = temp
+      //变异
+      //每个子实例都有可能基因（路径）多次变换
+      for (let j = 0; j < sonNumber; j++) {
+        if (Math.random() < mutationRate) {
+          for (let exchangeTime = Math.floor(Math.random() * pointsNumber + 1); exchangeTime > 0; exchangeTime--) {
+            const changePointA = Math.floor(Math.random() * pointsNumber)
+            const changePointB = Math.floor(Math.random() * pointsNumber)
+            const temp = sonGroup[j].route[changePointA]
+            sonGroup[j].route[changePointA] = sonGroup[j].route[changePointB]
+            sonGroup[j].route[changePointB] = temp
+          }
         }
       }
+      //计算子种群的各项参数
+      calculateGroup(sonGroup)
+      //合入原种群
+      sonGroup.forEach(v => groups.push(v))
+      sonGroup.splice(0)
+      //选优
+      groups.sort((a, b) => b.surviveProbability - a.surviveProbability)
+      //丢弃后面的
+      groups.splice(groupsNumber)
     }
-  }
-  //对生成的groups进行排序
-  groups.sort((a, b) => b.surviveProbability - a.surviveProbability);
-  message.value = `计算完成，在经过${iterationTimes}次的迭代后，最终的最短路径如下，总路程长度为${groups[0].distance}`
-  buttonDisabled.value = true
-  buttonMessage.value = "计算完成"
-  canvas.value.updateCanvas(points, groups[0].route)
+    message.value = `计算完成，在经过${iterationTimes}次的迭代后，最终的最短路径如下，总路程长度为${groups[0].distance}`
+    buttonDisabled.value = true
+    buttonMessage.value = "以新的迭代次数重新计算"
+    canvas.value.updateCanvas(points, groups[0].route)
+    buttonDisabled.value = false
+  }, 10)
 }
 function refresh() {
   location.reload();
+}
+function backToInit() {
+  groups = cloneDeep(initGroup)
+  message.value = "初始化后的种群中，存活率最高的一组如下，总路程长度为" + groups[0].distance;
+  canvas.value.updateCanvas(points, groups[0].route)
 }
 const message = ref("正在进行初始种群生成……");
 const buttonDisabled = ref(true);
@@ -178,7 +197,7 @@ onMounted(() => {
   setTimeout(() => {
     message.value = "初始化后的种群中，存活率最高的一组如下，总路程长度为" + groups[0].distance;
     buttonDisabled.value = false;
-  }, 4000);
+  }, 3000);
 });
 </script>
 
@@ -195,7 +214,8 @@ onMounted(() => {
 <Canvas ref="canvas" :width="width" :height="height" :points="points" :firstRoute="firstRoute" />
   <div>
     <button @click="refresh">重新生成（刷新）</button>
-    <button @click="calculate" :disabled="buttonDisabled" v-text="buttonMessage"></button>
+    <button @click="backToInit">恢复初始</button>
+    <button @click="calculate" :disabled="buttonDisabled">{{buttonMessage}}</button>
   </div>
 </template>
 
